f6d4793b1ef03a7962690baafa58ea59f017408c
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-19 21:36:53 -0600

    feat: created page by owner and admin

---
+++ app/(dashboard)/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+    </div>
+  )
+}
---
+++ app/(dashboard)/owner/page.tsx
@@ -0,0 +1,7 @@
+export default function OwnerPage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-2xl font-bold">Owner Dashboard</h1>
+    </div>
+  )
+}
