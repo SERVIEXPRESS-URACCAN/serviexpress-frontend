4d12882f35ae039fcc2daf548f81873ec3e86835
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-19 23:12:41 -0600

    feat: agregar página de negocios con diseño básico

---
+++ app/(dashboard)/admin/business/page.tsx
@@ -0,0 +1,7 @@
+export default function BusinessPage() {
+  return (
+    <div className="flex flex-col flex-1  bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-2xl font-bold">Negocios</h1>
+    </div>
+  )
+}
