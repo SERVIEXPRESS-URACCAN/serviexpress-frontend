2c17d9ab5863c1ba1f3334de1b8eb87e9c60d992
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-22 00:01:34 -0600

    feat: corregir la asignación de API_URL y simplificar el servicio de inicio de sesión

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,36 +1,22 @@
-import type {
-  LoginResponse,
-} from "@/types/auth.types";
+import type { LoginResponse } from '@/types/auth.types'
 
-import type {
-  LoginType,
-} from "@/schemas/login.schema";
+import { API_URL } from '@/config/config'
+import type { LoginType } from '@/schemas/login.schema'
 
-export async function loginService(
-  data: LoginType
-): Promise<LoginResponse> {
-  const response =
-    await fetch(
-      `${process.env.API_URL}/auth/login`,
-      {
-        method: "POST",
+export async function loginService(data: LoginType): Promise<LoginResponse> {
+  const response = await fetch(`${API_URL}/auth/login`, {
+    method: 'POST',
 
-        headers: {
-          "Content-Type":
-            "application/json",
-        },
+    headers: {
+      'Content-Type': 'application/json'
+    },
 
-        body: JSON.stringify(
-          data
-        ),
-      }
-    );
+    body: JSON.stringify(data)
+  })
 
   if (!response.ok) {
-    throw new Error(
-      "Credenciales inválidas"
-    );
+    throw new Error('Credenciales inválidas')
   }
 
-  return response.json();
+  return response.json()
 }
