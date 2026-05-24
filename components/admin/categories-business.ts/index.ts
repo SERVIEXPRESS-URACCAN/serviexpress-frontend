a4e2abedcf175bd24ed13f40c19fc98bb8cf2619
Chow <chowduarte@gmail.com>
2026-05-23 21:54:55 -0600

    feat: agregar archivo de índice para componentes de categorías de negocio

---
+++ components/admin/categories-business.ts/index.ts
@@ -0,0 +1,8 @@
+export * from "./categories-business-table";
+export * from "./categories-business-form";
+
+export * from "./create-categories-business-dialog";
+export * from "./edit-catergories-business-dialog";
+export * from "./delete-categories-business";
+
+export * from "./categories-business-actions";
