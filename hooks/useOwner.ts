e4d4894ca60fc6c9a0ac41ec6f98f73116edcceb
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-24 20:38:01 -0600

    feat: refactor useOwners hook to improve fetchOwners function and structure

--- hooks/useOwner.ts
+++ hooks/useOwner.ts
@@ -1,5 +1,3 @@
-'use client'
-
 import { getOwner } from '@/services/owner.service'
 import { Owner } from '@/types/owner.types'
 import { useEffect, useState } from 'react'
@@ -7,20 +5,29 @@ import { useAuth } from './useAuth'
 
 export const useOwners = () => {
   const { session } = useAuth()
+
   const [owners, setOwners] = useState<Owner[]>([])
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
+  const fetchOwners = async () => {
     if (!session?.accessToken) return
 
-    const fetchOwners = async () => {
-      const data = await getOwner(session.accessToken)
-      setOwners(data)
+    try {
+      const response = await getOwner(session.accessToken)
+
+      setOwners(response)
+    } finally {
       setLoading(false)
     }
+  }
 
+  useEffect(() => {
     fetchOwners()
   }, [session])
 
-  return { owners, loading }
+  return {
+    owners,
+    loading,
+    fetchOwners
+  }
 }
