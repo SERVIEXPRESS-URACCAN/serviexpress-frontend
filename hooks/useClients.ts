93097ac1aeb867ff7948000d47b7da613050d376
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-05-31 19:43:26 -0600

    fix: fixinf paginatio en client

--- hooks/useClients.ts
+++ hooks/useClients.ts
@@ -4,19 +4,21 @@ import { useEffect, useState } from 'react'
 import { getClientes } from '@/services/clients.service'
 import { ClientsResponse } from '@/types/clients'
 import { useAuth } from './useAuth'
+import { useSearchParams } from 'next/navigation'
 
 export const useClients = () => {
-    const { session } = useAuth()
-  
+  const { session } = useAuth()
+  const searchParams = useSearchParams()
+  const page = Number(searchParams.get('page') || 1)
+
   const [clients, setClients] = useState<ClientsResponse | null>(null)
   const [loading, setLoading] = useState(true)
 
   const fetchClients = async () => {
     if (!session?.accessToken) return
-    
-
     try {
-      const response = await getClientes(session.accessToken)
+      setLoading(true)
+      const response = await getClientes(session.accessToken, page)
 
       setClients(response)
     } finally {
@@ -26,7 +28,7 @@ export const useClients = () => {
 
   useEffect(() => {
     fetchClients()
-  }, [session])
+  }, [session, page])
 
   return {
     clients,
