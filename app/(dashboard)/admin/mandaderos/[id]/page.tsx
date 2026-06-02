cb4f82d1ae9e2b96702555c238e813bd473f3145
Chow <chowduarte@gmail.com>
2026-06-02 17:44:35 -0600

    feat: mejorar diseño de la página de detalle de mandadero y eliminar que se muestre el color en   tabla de mandaderos

--- app/(dashboard)/admin/mandaderos/[id]/page.tsx
+++ app/(dashboard)/admin/mandaderos/[id]/page.tsx
@@ -28,67 +28,99 @@ export default async function MandaderoDetailPage({
       </div>
 
       <div
-        className="grid gap-6 items-start"
+        className="grid gap-6 items-stretch "
         style={{
           gridTemplateColumns: "1fr 1fr 1fr",
         }}
       >
         <Card>
-          <CardContent className="pt-6 space-y-4">
-            <div className="flex justify-center">
-              <p className="text-base  mb-2">Foto de perfil</p>
+          <CardContent className="p-6 space-y-4">
+            <div className="flex flex-col items-center gap-2">
+              <div className="w-40 aspect-square rounded-full overflow-hidden border-2 border-border bg-muted shrink-0 mx-auto">
+                {" "}
+                {mandadero.user?.profile?.image ? (
+                  <Image
+                    src={`${API_IMG_URL}/uploads/profiles/${mandadero.user.profile.image}`}
+                    alt="Foto de perfil"
+                    fill
+                    className="object-cover"
+                    unoptimized
+                  />
+                ) : (
+                  <div className="w-full h-full flex items-center justify-center">
+                    <span className="text-3xl text-muted-foreground">
+                      {mandadero.user?.profile?.name?.charAt(0).toUpperCase() ??
+                        "?"}
+                      {mandadero.user?.profile?.lastName
+                        ?.charAt(0)
+                        .toUpperCase() ?? "?"}
+                    </span>
+                  </div>
+                )}
+              </div>
             </div>
 
-            <div className="space-y-3 text-sm">
-              <div className="space-y-1">
-                <label className="text-lg font-bold ">Nombre y Apellido</label>
+            <div className="space-y-6">
+              <div className="space-y-3">
+                <label className="text-lg font-bold block mb-1">Nombre</label>
                 <input
-                  value={`${mandadero.user?.profile?.name} ${mandadero.user?.profile?.lastName}`}
+                  value={`${mandadero.user?.profile?.name} `}
                   disabled
-                  className="w-full rounded-md border px-3 py-2 text-base bg-muted  cursor-not-allowed"
+                  className="w-full rounded-md border px-4 py-2 text-base bg-muted  cursor-not-allowed"
                 />
               </div>
-              <div>
-                <label className="text-lg font-bold">Teléfono</label>
+              <div className="space-y-3">
+                <label className="text-lg font-bold block mb-1">Apellido</label>
+                <input
+                  value={` ${mandadero.user?.profile?.lastName}`}
+                  disabled
+                  className="w-full rounded-md border px-4 py-2 text-base bg-muted  cursor-not-allowed"
+                />
+              </div>
+              <div className="space-y-3">
+                <label className="text-lg font-bold block mb-2">Teléfono</label>
                 <input
                   value={mandadero.user?.profile?.cellphone}
                   disabled
-                  className="w-full rounded-md border px-3 py-2 text-base bg-muted  cursor-not-allowed"
+                  className="w-full rounded-md border px-4 py-2 text-base bg-muted  cursor-not-allowed"
                 />
               </div>
-              <div>
-                <label className="text-lg font-bold">Email</label>
+              <div className="space-y-3">
+                <label className="text-lg font-bold block mb-2">Email</label>
                 <input
                   value={mandadero.user?.email}
                   disabled
-                  className="w-full rounded-md border px-3 py-2 text-base bg-muted  cursor-not-allowed"
+                  className="w-full rounded-md border px-4 py-2 text-base bg-muted  cursor-not-allowed"
                 />
               </div>
-              <div>
-                <label className="text-lg font-bold">Genero</label>
+              <div className="space-y-3">
+                <label className="text-lg font-bold block mb-2">Genero</label>
                 <input
                   value={mandadero.user?.profile?.gender?.name ?? "-"}
                   disabled
-                  className="w-full rounded-md border px-3 py-2 text-base bg-muted  cursor-not-allowed"
+                  className="w-full rounded-md border px-4 py-2 text-base bg-muted  cursor-not-allowed"
                 />
               </div>
             </div>
           </CardContent>
         </Card>
 
-        <div className="space-y-4">
-          <Card>
+        <div className="grid grid-cols-[3fr_7fr] gap-6 h-full">
+          {" "}
+          <Card className="h-full">
+            {" "}
             <CardHeader className="pb-2">
               <CardTitle className="text-lg font-bold uppercase tracking-wide ">
                 Estado
               </CardTitle>
             </CardHeader>
-            <CardContent className="grid grid-cols-2 gap-3 text-sm">
-              <div>
+            <CardContent className="grid grid-cols-2 gap-4 py-4 text-sm">
+              {" "}
+              <div className="flex flex-col items-center gap-4">
                 <p className="text-lg font-bold mb-3">Estado</p>
 
                 <span
-                  className={`inline-flex items-center px-2 py-0.5 rounded-full text-xl font-semibold ${
+                  className={`inline-flex items-center justify-center w-full px-4 py-4 rounded-xl text-xl font-bold ${
                     mandadero.isActive
                       ? "bg-emerald-100 text-emerald-800 dark:bg-emerald-950 dark:text-emerald-400"
                       : "bg-slate-100 text-slate-800 dark:bg-slate-800 dark:text-slate-400"
@@ -97,10 +129,10 @@ export default async function MandaderoDetailPage({
                   {mandadero.isActive ? "Activo" : "Inactivo"}
                 </span>
               </div>
-              <div>
+              <div className="flex flex-col items-center gap-4">
                 <p className="text-lg font-bold  mb-3 ">Disponibilidad</p>
                 <span
-                  className={`inline-flex items-center px-2 py-0.5 rounded-full text-xl font-semibold ${
+                  className={`inline-flex items-center justify-center w-full px-4 py-4 rounded-xl text-xl font-bold ${
                     mandadero.available
                       ? "bg-green-100 text-green-800 dark:bg-green-900/30 dark:text-green-400"
                       : "bg-red-100 text-red-800 dark:bg-red-900/30 dark:text-red-400"
@@ -111,14 +143,13 @@ export default async function MandaderoDetailPage({
               </div>
             </CardContent>
           </Card>
-
-          <Card>
+          <Card className="h-full">
             <CardHeader className="pb-2">
               <CardTitle className="text-lg font-bold uppercase tracking-wide">
                 Motocicleta
               </CardTitle>
             </CardHeader>
-            <CardContent className="grid grid-cols-2 gap-3 text-sm">
+            <CardContent className="h-full flex flex-col justify-center gap-4 text-sm">
               <div>
                 <label className="text-lg font-bold ">Placa</label>
                 <input
