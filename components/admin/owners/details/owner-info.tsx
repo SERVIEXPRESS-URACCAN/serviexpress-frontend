9b69e81b1ff2acefa2d19cbede8d8db121a82214
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-06-13 10:40:39 -0600

    fix: mejorar el componente OwnerInfo para incluir un avatar y centrar el título

--- components/admin/owners/details/owner-info.tsx
+++ components/admin/owners/details/owner-info.tsx
@@ -1,5 +1,7 @@
 import { InfoField } from '@/components/shared/Info-field'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { API_IMG_URL } from '@/config/config'
 import { Owner } from '@/types/owner.types'
 
 type Props = {
@@ -11,11 +13,27 @@ export const OwnerInfo = ({ owner }: Props) => {
     <Card className="w-ful">
       {' '}
       <CardHeader>
-        <CardTitle className="text-lg font-bold uppercase tracking-wide">
+        <CardTitle className="text-lg font-bold uppercase tracking-wide text-center">
           Información Personal
         </CardTitle>
       </CardHeader>
       <CardContent className="space-y-6">
+        <div className="flex justify-center">
+          <Avatar className="h-32 w-32">
+            <AvatarImage
+              src={
+                owner.user?.profile?.profileImage
+                  ? `${API_IMG_URL}/uploads/profile/${owner.user.profile.profileImage}`
+                  : undefined
+              }
+              alt={`${owner.user?.profile?.name} ${owner.user?.profile?.lastName}`}
+            />
+            <AvatarFallback className="text-4xl font-bold">
+              {owner.user?.profile?.name?.charAt(0).toUpperCase() ?? '?'}
+              {owner.user?.profile?.lastName?.charAt(0).toUpperCase() ?? '?'}
+            </AvatarFallback>
+          </Avatar>
+        </div>
         <InfoField label="Nombre" value={owner.user?.profile?.name} />
 
         <InfoField label="Apellido" value={owner.user?.profile?.lastName} />
