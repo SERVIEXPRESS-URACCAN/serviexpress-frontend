4ef4a18ca617b4ac10368cecbf016c2e4f9c2e95
Bryan_Alvarado <117047716+Bryan18-Alvarado@users.noreply.github.com>
2026-06-16 10:04:47 -0600

    delete console.log

--- auth.ts
+++ auth.ts
@@ -61,7 +61,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       }
 
       if (trigger === 'update' && sessionData) {
-        console.log("JWT UPDATE", sessionData)
         return {
           ...token,
           accessToken: sessionData.accessToken,
