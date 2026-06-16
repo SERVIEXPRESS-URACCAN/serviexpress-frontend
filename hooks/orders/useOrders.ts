021dc4fa397a9797e86adaae654ceead526faf4f
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-06-16 12:46:23 -0600

    feat: agregar hook useOrders para gestionar la obtención de pedidos

---
+++ hooks/orders/useOrders.ts
@@ -0,0 +1,39 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
+
+import { getOrders } from '@/services/orders.service'
+import { OrderResponse } from '@/types/order.type'
+
+export const useOrders = () => {
+  const [orders, setOrders] = useState<OrderResponse | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  const searchParams = useSearchParams()
+  const search = searchParams.get('search') || undefined
+
+  const fetchOrders = useCallback(async () => {
+    try {
+      setLoading(true)
+
+      const response = await getOrders(1, 10, search)
+
+      console.log('[useOrders] Orders:', response)
+
+      setOrders(response)
+    } finally {
+      setLoading(false)
+    }
+  }, [search])
+
+  useEffect(() => {
+    fetchOrders()
+  }, [fetchOrders])
+
+  return {
+    orders,
+    loading,
+    fetchOrders
+  }
+}
