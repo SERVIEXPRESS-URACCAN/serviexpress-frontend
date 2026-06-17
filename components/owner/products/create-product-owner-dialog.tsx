ea8163f01585a1f80c4afd4fc091d7ba8ccf4d74
Chow <chowduarte@gmail.com>
2026-06-17 16:22:41 -0600

    feat: implementar gestión de productos del propietario con creación, edición y eliminación

---
+++ components/owner/products/create-product-owner-dialog.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { useAuth } from '@/hooks/useAuth'
+import { CreateProductInput } from '@/schemas/products.schema'
+
+import { CategoryProduct } from '@/types/categories-products'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { CreateProductOwnerForm } from './create-product-owner-form'
+import { createOwnerProduct } from '@/services/owner/product-owner.service'
+
+type Props = {
+  categories: CategoryProduct[]
+  onSuccessAction?: () => Promise<void>
+}
+
+export const CreateProductOwnerDialog = ({
+  categories,
+  onSuccessAction,
+}: Props) => {
+  const { session } = useAuth()
+  const [open, setOpen] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const handleOpenChange = (value: boolean) => {
+    if (!value) setError(null)
+    setOpen(value)
+  }
+
+  const onSubmit = async (data: CreateProductInput) => {
+    if (!session) return
+    try {
+      setIsSubmitting(true)
+      const formData = new FormData()
+      formData.append('name', data.name)
+      formData.append('price', String(data.price))
+
+      data.categoryIds.forEach((id) => {
+        formData.append('categoryIds', String(id))
+      })
+      if (data.description) formData.append('description', data.description)
+      if (data.image) formData.append('image', data.image)
+
+      await createOwnerProduct(formData)
+      await onSuccessAction?.()
+      setOpen(false)
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : 'Error creando el producto'
+      setError(message)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogTrigger asChild>
+        <Button>Agregar Producto</Button>
+      </DialogTrigger>
+      <DialogContent
+        className='max-h-[90vh] overflow-y-auto'
+        onInteractOutside={(e) => e.preventDefault()}
+      >
+        <DialogHeader>
+          <DialogTitle>Crear Producto</DialogTitle>
+          <DialogDescription>
+            Completa los datos para agregar un nuevo producto.
+          </DialogDescription>
+        </DialogHeader>
+        <CreateProductOwnerForm
+          key={open ? 'open' : 'closed'}
+          categories={categories}
+          onSubmitAction={onSubmit}
+          isSubmitting={isSubmitting}
+          error={error}
+        />
+      </DialogContent>
+    </Dialog>
+  )
+}
---
+++ components/owner/products/edit-product-owner-dialog.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import { useAuth } from '@/hooks/useAuth'
+import { UpdateProductInput } from '@/schemas/products.schema'
+import { CategoryProduct } from '@/types/categories-products'
+import { Product } from '@/types/products.type'
+
+import { useState } from 'react'
+import { EditProductOwnerForm } from './edit-product-owner-form'
+import { updateOwnerProduct } from '@/services/owner/product-owner.service'
+
+type Props = {
+  product: Product
+  categories: CategoryProduct[]
+  open: boolean
+  onOpenChangeAction: (open: boolean) => void
+  onSuccessAction?: () => Promise<void>
+}
+
+export const EditProductOwnerDialog = ({
+  product,
+  categories = [],
+  open,
+  onOpenChangeAction,
+  onSuccessAction,
+}: Props) => {
+  const { session } = useAuth()
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const handleOpenChange = (value: boolean) => {
+    if (!value) setError(null)
+    onOpenChangeAction(value)
+  }
+
+  const onSubmit = async (data: UpdateProductInput) => {
+    const formData = new FormData()
+
+    for (const pair of formData.entries()) {
+      console.log(pair[0], pair[1])
+    }
+
+    if (!session) return
+
+    try {
+      setIsSubmitting(true)
+      const formData = new FormData()
+
+      formData.append('name', data.name || '')
+      formData.append('price', String(data.price))
+      ;(data.categoryIds ?? []).forEach((id) => {
+        formData.append('categoryIds', String(id))
+      })
+
+      formData.append('status', data.status ? 'true' : 'false')
+      if (data.description) formData.append('description', data.description)
+      if (data.image) formData.append('image', data.image)
+
+      await updateOwnerProduct(product.id, formData)
+      await onSuccessAction?.()
+      onOpenChangeAction(false)
+    } catch (error) {
+      const message =
+        error instanceof Error
+          ? error.message
+          : 'Error actualizando el producto'
+      setError(message)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogContent
+        className='max-h-[90vh] overflow-y-auto'
+        onInteractOutside={(e) => e.preventDefault()}
+      >
+        <DialogHeader>
+          <DialogTitle>Editar Producto</DialogTitle>
+          <DialogDescription>Editar Producto</DialogDescription>
+        </DialogHeader>
+        <EditProductOwnerForm
+          product={product}
+          categories={categories}
+          onSubmitAction={onSubmit}
+          isSubmitting={isSubmitting}
+          error={error}
+        />
+      </DialogContent>
+    </Dialog>
+  )
+}
